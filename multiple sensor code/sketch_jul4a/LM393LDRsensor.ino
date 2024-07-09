86e20596d8757f92217d2d2e3d618e0d9c514977
MannPatel2021 <165867168+MannPatel2021@users.noreply.github.com>
2024-07-09 18:09:28 +0530

    Delete multiple sensor code directory

--- multiple sensor code/sketch_jul4a/LM393LDRsensor.ino
+++
@@ -1,9 +0,0 @@
-/*
------FUNCTION FOR LM393 LDR SENSOR SENSOR-----
-*/
-void LM383LDRSensor(){
-  unsigned int SensorValue1 = analogRead(LDR);
-  Serial.print("LDR Sensor : ");
-  Serial.println(SensorValue1);
-  delay(1000);
-}
